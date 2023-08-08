6ada1070a265d9a343805da83ab1a178553b177f
Almario Guzman <97215500+bxste@users.noreply.github.com>
2023-08-08 23:41:37 +0800

    Update fiction.component.ts

--- app/genre/fiction/fiction.component.ts
+++ app/genre/fiction/fiction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiserviceService } from 'src/app/apiservice.service';
 
 @Component({
   selector: 'app-fiction',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./fiction.component.css']
 })
 export class FictionComponent {
+  readDataBook4: any[] = [];
+  constructor(private service:ApiserviceService){}
+
+  ngOnInit(): void{
+
+    this.service.getAllDataBook4().subscribe((res)=>{
+      console.log(res,"res==>");
+      this.readDataBook4 = res.data;
+    });
+  }
 
 }
